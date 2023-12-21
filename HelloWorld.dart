2cf77680938988c1df4d61ced72910d3e390513d
Nguyen Van Thuan <ttctde@gmail.com>
2023-12-21 23:37:41 +0700

    Hello World

---
+++ HelloWorld.dart
@@ -0,0 +1,12 @@
+void main() {
+  // This is my first line of code
+  print('hello ${'world'}'); // hello world
+
+  print("This is my first application");
+
+  //Performing operation
+  print(12 / 4); // 3.0 type float || number;
+
+  // Printing out boolean value
+  print(false);
+}
