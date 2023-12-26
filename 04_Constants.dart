b1897157f7d79bcf155769672c2d5994e87cbae6
Nguyen Van Thuan <ttctde@gmail.com>
2023-12-26 23:45:45 +0700

    Constants

---
+++ 04_Constants.dart
@@ -0,0 +1,18 @@
+void main() {
+  // final
+  final cityName;
+  cityName = 12;
+
+  // name  = 'keyxk'  Throws an errors
+  final String contryName = 'Vietnamese';
+
+  // const
+  const PI = 3.14;
+
+  const double gravity = 9.8;
+}
+
+class Circle {
+  final color = 'Red';
+  static const PI = 3.14;
+}
