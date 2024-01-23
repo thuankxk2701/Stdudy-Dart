f5e465e4e53da4edd2c76e1dc4ad1f63d0190129
Nguyen Van Thuan <ttctde@gmail.com>
2024-01-23 15:31:50 +0700

    Inheritance with Constructors

---
+++ 24_InheritanceWithConstructors.dart
@@ -0,0 +1,43 @@
+// Objectives
+// 1. Inheritance with Default Constructors and Parameterised Constructor
+// 2. Inheritance with Named Constructor
+
+void main() {
+  var dog1 = Dog("Labrador", "Black");
+
+  print("");
+
+  var dog2 = Dog("Pug", "Brown");
+
+  print("");
+
+  var dog3 = Dog.myNamedConstructor("German Shepherd", "Black-Brown");
+}
+
+class Animal {
+  String? color;
+
+  Animal(String color) {
+    this.color = color;
+    print("Animal class constructor");
+  }
+
+  Animal.myAnimalNamedConstructor(String color) {
+    print("Animal class named constructor");
+  }
+}
+
+class Dog extends Animal {
+  String? breed;
+
+  Dog(String breed, String color) : super(color) {
+    this.breed = breed;
+    print("Dog class constructor");
+  }
+
+  Dog.myNamedConstructor(String breed, String color)
+      : super.myAnimalNamedConstructor(color) {
+    this.breed = breed;
+    print("Dog class Named Constructor");
+  }
+}
